e0c8848f0cec71909ed06d7a64b73c5131f7d764
Ishan Mittal <ishanmittal120@gmail.com>
2024-01-05 13:39:56 +0530

    webhook logger added

--- src/main/java/ui_framework/pages/HomePage.java
+++ src/main/java/ui_framework/pages/HomePage.java
@@ -32,7 +32,7 @@ public class HomePage {
     }
 
     public void chooseHeaderOption(String inputValue){
-        logger.info("adding webhook");
+        logger.info("adding webhook static ip");
         DriverManager.getWebDriverManager().getWebDriver().findElement(By.xpath(String.format(HEADER_OPTION,inputValue))).click();
     }
 
