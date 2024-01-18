0f0ae015d239a05f35465985f5c97eac60d86db7
Ishan Mittal <ishanmittal120@gmail.com>
2024-01-18 15:10:05 +0530

    headless mode added

--- src/main/java/ui_framework/DriverPackage/DriverFactory.java
+++ src/main/java/ui_framework/DriverPackage/DriverFactory.java
@@ -85,7 +85,7 @@ public class DriverFactory {
     private WebDriver getChromeDriverInstance(){
         ChromeOptions options = new ChromeOptions();
         options.setBrowserVersion(ConfigReader.getConfigReader().getProperty("browserVersion"));
-        options.addArguments("--remote-debugging-pipe");
+        options.addArguments("--headless","--window-size=1920,1200");
         WebDriver driver = new ChromeDriver(options);
         return driver;
     }
